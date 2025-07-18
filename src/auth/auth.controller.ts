0c781742b8cd749945c416068324b1e5166e7284
이현종 <ehdclr@gractor.com>
2025-07-18 17:57:12 +0900

    feat: auth controller 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { QueryBus } from '@nestjs/cqrs';
+import { Body } from '@nestjs/common';
+import { LoginRequestSchema, LoginRequest } from '@/auth/dto/auth.request';
+import { ZodValidationPipe } from '@/common/pipes/zod-validation.pipe';
+import { ApiResponse } from '@/common/interfaces/response.interface';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  //TODO 로그인
+  @Post('login')
+  @UsePipes(new ZodValidationPipe(LoginRequestSchema))
+  login(@Body() loginDto: LoginRequest): Promise<ApiResponse<LoginResponse>> {
+    return this.commandBus.execute(new LoginCommand(loginDto));
+  }
 }
